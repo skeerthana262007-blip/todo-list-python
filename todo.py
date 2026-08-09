662259e2b49dd15184fe9823a8480643802e053c
Keerthana.S <skeerthana262007@gmail.com>
2026-08-09 09:50:26 +0530

    Initial commit

---
+++ todo.py
@@ -0,0 +1,45 @@
+tasks = []
+
+while True:
+    print("\n===== TO-DO LIST =====")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Delete Task")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        task = input("Enter your task: ")
+        tasks.append(task)
+        print("Task added successfully!")
+
+    elif choice == "2":
+        if len(tasks) == 0:
+            print("No tasks available.")
+        else:
+            print("\nYour Tasks:")
+            for i, task in enumerate(tasks, start=1):
+                print(i, ".", task)
+
+    elif choice == "3":
+        if len(tasks) == 0:
+            print("No tasks to delete.")
+        else:
+            for i, task in enumerate(tasks, start=1):
+                print(i, ".", task)
+
+            number = int(input("Enter task number to delete: "))
+
+            if 1 <= number <= len(tasks):
+                deleted = tasks.pop(number - 1)
+                print("Deleted:", deleted)
+            else:
+                print("Invalid task number.")
+
+    elif choice == "4":
+        print("Thank you for using To-Do List!")
+        break
+
+    else:
+        print("Invalid choice. Try again.")
